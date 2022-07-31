7969de4fcd851c226fd9b5246b1b141f8ae2c7fb
furiousnur <furiousnur@gmail.com>
2022-07-31 23:28:33 +0600

    type check for each type of variables, array object numer string check for push

--- src/script.ts
+++ src/script.ts
@@ -8,4 +8,27 @@ function multiply(a:number, b:number){
     return a * b;
 }
 
-console.log(multiply(6,34))
+console.log(multiply(6,34));
+
+//Array
+let fruits = ['apple', 'banana', 'orange'];
+// fruits.push(3432); //'number' is not assignable to parameter of type 'string'
+fruits.push('Mango');
+console.log(fruits);
+
+let mixed = ['aaa', true, 3, {}]
+
+mixed.push({
+   name: 'Bangladesh' //declare object{} in parameter before push object
+});
+
+console.log(mixed);
+
+//object
+let user = {
+    name: 'A',
+    age: 34,
+    id: 1
+}
+
+console.log(user);
