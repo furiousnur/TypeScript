375e29df27d7ff06dbf330ad6e99206c85d59c9f
furiousnur <furiousnur@gmail.com>
2022-08-02 00:08:31 +0600

    function type

--- output/script.js
+++ output/script.js
@@ -1,53 +1,13 @@
 "use strict";
-let playerName = "Mashrafi";
-console.log(playerName);
-const a = 24;
-console.log(a);
-//Function
-function multiply(a, b) {
-    return a * b;
-}
-console.log(multiply(6, 34));
-//Array
-let fruits = ['apple', 'banana', 'orange'];
-// fruits.push(3432); //'number' is not assignable to parameter of type 'string'
-fruits.push('Mango');
-console.log(fruits);
-let mixed = ['aaa', true, 3, {}];
-mixed.push({
-    name: 'Bangladesh' //declare object{} in parameter before push object
-});
-console.log(mixed);
-//object
-let user = {
-    name: 'A',
-    age: 34,
-    id: 1
+//function type
+let myFunction;
+myFunction = () => {
+    console.log("Hello");
 };
-console.log(user);
-//specify the variable type wise
-let stringVariable;
-stringVariable = "34";
-let numberVariable;
-numberVariable = 34;
-//Array
-let arrayVariable = []; //array type
-arrayVariable.push("done");
-let bothArray = []; //union type
-// bothArray.push(34,"asdfsdf",false);
-bothArray.push(34, "asdfsdf");
-//Object //explicit type
-let info;
-info = {
-    name: 'AAA',
-    phone: 34234324,
-    // adult: "asdfasdf",
-    adult: true,
+//props variable type
+const parFunc = (a, b, c) => {
+    console.log(`Hello ${a} ${b} ${c}`); //can't show c cause of optional variable
 };
-//Dynamic type or any type
-let any;
-any = 4590;
-let anyArray = [];
-anyArray.push(3432);
-anyArray.push("asdfasdf");
-console.log(anyArray);
+// parFunc(3,4);
+parFunc("Yes", "No");
+// void and undefined are not same.
--- src/script.ts
+++ src/script.ts
@@ -1,72 +1,13 @@
-let playerName="Mashrafi"
-console.log(playerName);
-const a = 24;
-console.log(a);
+//function type
+let myFunction: Function;
+ myFunction = () => {
+    console.log("Hello");
+};
 
-//Function
-function multiply(a:number, b:number){
-    return a * b;
+ //props variable type
+const parFunc = (a: string, b:string, c?:string) =>{
+    console.log(`Hello ${a} ${b} ${c}`); //can't show c cause of optional variable
 }
-
-console.log(multiply(6,34));
-
-//Array
-let fruits = ['apple', 'banana', 'orange'];
-// fruits.push(3432); //'number' is not assignable to parameter of type 'string'
-fruits.push('Mango');
-console.log(fruits);
-
-let mixed = ['aaa', true, 3, {}]
-
-mixed.push({
-   name: 'Bangladesh' //declare object{} in parameter before push object
-});
-
-console.log(mixed);
-
-//object
-let user = {
-    name: 'A',
-    age: 34,
-    id: 1
-}
-
-console.log(user);
-
-//specify the variable type wise
-let stringVariable: string;
-stringVariable = "34";
-
-let numberVariable: number;
-numberVariable = 34;
-
-//Array
-let arrayVariable: string[] = []; //array type
-arrayVariable.push("done");
-
-let bothArray: (string|number)[] = []; //union type
-// bothArray.push(34,"asdfsdf",false);
-bothArray.push(34,"asdfsdf");
-
-//Object //explicit type
-let info: {
-    name: string,
-    phone: number,
-    adult: boolean
-}
-
-info = {
-    name: 'AAA',
-    phone: 34234324,
-    // adult: "asdfasdf",
-    adult: true,
-}
-
-//Dynamic type or any type
-let any: any;
-any = 4590;
-
-let anyArray:any[] = [];
-anyArray.push(3432);
-anyArray.push("asdfasdf");
-console.log(anyArray);
+// parFunc(3,4);
+parFunc("Yes", "No");
+// void and undefined are not same.
